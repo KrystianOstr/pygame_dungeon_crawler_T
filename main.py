44eb12788a7aa4fd30c360829c8b2867a98ee915
Krystian <krystian.ostrogorski@gmail.com>
2025-06-19 14:53:24 +0200

    added all characters and bow

--- main.py
+++ main.py
@@ -1,25 +1,129 @@
 import pygame
+import constants
+from character import Character
+from weapon import Weapon
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
+pygame.display.set_caption('Dungeon Crawler')
+
+
+#creating clock for FPS
+clock = pygame.time.Clock()
+
+#define player movement variables
+moving_left = False
+moving_right = False
+moving_up = False
+moving_down = False
+
+# helper function to scale image
+def scale_img(image, scale):
+    w = image.get_width()
+    h = image.get_height()
+
+    return pygame.transform.scale(image,(w*scale, h*scale))
+
+# load weapon image
+bow_image = scale_img(pygame.image.load('assets/images/weapons/bow.png').convert_alpha(), constants.WEAPON_SCALE)
+arrow_image = scale_img(pygame.image.load('assets/images/weapons/arrow.png').convert_alpha(), constants.WEAPON_SCALE)
+
+
+#load character images
+mob_animation = []
+mob_types = ["elf", "imp", "skeleton", 'goblin', 'muddy', 'tiny_zombie', 'big_demon']
+
+for mob in mob_types:
+    animation_types = ["idle", "run"]
+    #load images
+    animation_list = []
+    for animation in animation_types:
+        #reset temprorary list of images
+        temp_list = []
+        for i in range(4):
+            #player image
+            img = pygame.image.load(f'assets/images/characters/{mob}/{animation}/{i}.png').convert_alpha()
+            img = scale_img(img, constants.SCALE)
+            temp_list.append(img)
+
+        animation_list.append(temp_list)
+    mob_animation.append(animation_list)
+
+# create player
+player = Character(100,100, mob_animation, 0)
+
+# create weapon
+bow = Weapon(bow_image)
 
 
 #main game loop
 run = True
 
 while run:
+
+    clock.tick(constants.FPS)
+
+    screen.fill(constants.BG)
+
+    #calculate player movement
+    dx = 0
+    dy = 0
+    if moving_right == True:
+        dx = constants.SPEED
+    if moving_left == True:
+        dx = -constants.SPEED
+    if moving_up == True:
+        dy = -constants.SPEED
+    if moving_down == True:
+        dy = constants.SPEED
+
+
+    #move player
+    player.move(dx,dy)
+
+    #update player
+    player.update()
+    bow.update(player)
+
+    #draw player on the screen
+    player.draw(screen)
+    bow.draw(screen)
+
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+        #keyboard bytton pressed
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                run = False
+                run = False 
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True     
+            if event.key == pygame.K_w:
+                moving_up = True
+            if event.key == pygame.K_s:
+                moving_down = True
+
+                #keyboard bytton up
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False     
+            if event.key == pygame.K_w:
+                moving_up = False
+            if event.key == pygame.K_s:
+                moving_down = False
             
         
+    pygame.display.update()
 
 pygame.quit()
